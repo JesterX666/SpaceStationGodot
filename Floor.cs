8bced0b2fabcf8f02a8ce75a462a99e2087d5ca6
JesterX <mracicot@hotmail.com>
2023-11-02 22:58:59 -0400

    Updates

---
+++ Floor.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class Floor : Node3D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		PackedScene floor1 = ResourceLoader.Load<PackedScene>("res://Building/Floor/floor_tile_1.tscn");
+        PackedScene floor2 = ResourceLoader.Load<PackedScene>("res://Building/Floor/floor_tile_2.tscn");
+
+        //Godot.Node3D node;
+        Node3D node;
+        node = (Node3D) floor1.Instantiate();
+        node.Position = new Vector3(0, 0, 0);
+        this.AddChild(node);
+
+        node = (Node3D)floor2.Instantiate();
+        node.Position = new Vector3(1, 0, 0);
+        this.AddChild(node);
+
+        node = (Node3D)floor2.Instantiate();
+        node.Position = new Vector3(0, 0, 1);
+        this.AddChild(node);
+
+        node = (Node3D)floor1.Instantiate();
+        node.Position = new Vector3(1, 0, 1);
+        this.AddChild(node);
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+	{
+	}
+}
